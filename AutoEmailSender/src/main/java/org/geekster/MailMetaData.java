1dd317cc4077a28c1b9c795b141b85300c73beb3
msrwizard <117990812+msrwizard@users.noreply.github.com>
2023-06-30 01:18:41 +0530

    Add files via upload

---
+++ AutoEmailSender/src/main/java/org/geekster/MailMetaData.java
@@ -0,0 +1,13 @@
+package org.geekster;
+
+public class MailMetaData {
+
+    public static final String HostServer = "smtp.gmail.com";
+    public static final String port = "465";
+    public static final String sslProperty = "mail.smtp.ssl.enable";
+    public static final String authPerm = "mail.smtp.auth";
+    public static final String myUserMail = "msrwizard@gmail.com";
+    public static final String password = "eigytnuwrmmauzkg";
+    public static final String recieverEmail = "rjaideep.singh24@gmail.com";
+
+}
